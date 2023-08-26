508530c252ba240dafd12b834c8548c606b42af6
Rishabh Gupta <programmer.rishabh@gmail.com>
2023-08-26 19:27:36 +0530

    Initial Commit

---
+++ src/main/java/in/rishabh_gupta/YoutubeCrudApplication.java
@@ -0,0 +1,13 @@
+package in.rishabh_gupta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoutubeCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/rishabh_gupta/YoutubeCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package in.rishabh_gupta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubeCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
